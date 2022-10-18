99ea6e748dc8011051bd9f33393387f8cea6cf17
Deca <deca@admins-MacBook-Air.local>
2022-10-18 20:22:40 +0100

    First commit

---
+++ src/main/java/com/example/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,29 @@
+package com.example.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to CI/CD";
+    }
+
+//    echo "# github-actions-cicd" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/SuleimanIbrahiminuwa/github-actions-cicd.git
+//    git push -u origin main
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+}
